ef48d2d53b5937c7bc9d07893150dd6ddee803f5
AndreCypriano23 <andrecypriano23@outlook.com>
2022-04-13 20:35:46 -0300

    Primeiro Commit do Projeto

---
+++ FilmesApi/Controllers/CinemaController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CinemaController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public CinemaController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+  
+
+        [HttpPost]
+        public IActionResult AdicionaCinema([FromBody] CreateCinemaDto CinemaDto)
+        {
+            Cinema Cinema = _mapper.Map<Cinema>(CinemaDto);
+            _context.Cinemas.Add(Cinema);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaCinemasPorId), new { Id = Cinema.Id }, Cinema);
+        }
+
+        [HttpGet]
+        public IEnumerable<Cinema> RecuperaCinemas([FromQuery] string nomeDoFilme)
+        {
+            return _context.Cinemas;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaCinemasPorId(int id)
+        {
+            Cinema Cinema = _context.Cinemas.FirstOrDefault(Cinema => Cinema.Id == id);
+            if(Cinema != null)
+            {
+                ReadCinemaDto CinemaDto = _mapper.Map<ReadCinemaDto>(Cinema);
+                return Ok(CinemaDto);
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaCinema(int id, [FromBody] UpdateCinemaDto CinemaDto)
+        {
+            Cinema Cinema = _context.Cinemas.FirstOrDefault(Cinema => Cinema.Id == id);
+            if(Cinema == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(CinemaDto, Cinema);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaCinema(int id)
+        {
+            Cinema Cinema = _context.Cinemas.FirstOrDefault(Cinema => Cinema.Id == id);
+            if (Cinema == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(Cinema);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
---
+++ FilmesApi/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            //Algumas definições na hora de iniciar, como os modelos serão iniciados
+            //RELACIONAMENTO DE 1 PARA 1 E O ENDEREÇO DEVE EXISTIR ANTES
+            builder.Entity<Endereco>()
+                .HasOne(endereco => endereco.Cinema)//Endereço que tem um cinema
+                .WithOne(cinema => cinema.Endereco)//Cinema que tem um endereço
+                .HasForeignKey<Cinema>(cinema => cinema.EnderecoId);
+                //ESTOU EXPLICITANDO QUE O MEU ENDERECO TEM UM CINEMA
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
